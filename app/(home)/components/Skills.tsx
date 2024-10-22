a7c4095858e9cc6c0709e2a7c4d3ad5f9228e06d
preet006 <preetbhardwaj146@gmail.com>
2024-10-22 12:16:15 +0530

    update resume and skills and projects

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 import Title from "./Title";
 import { HoverEffect } from "@/app/components/ui/card-hover-effect";
-import { SiGit, SiJavascript, SiNextdotjs, SiPostgresql, SiPostman, SiReact, SiRedux, SiSupabase, SiTailwindcss } from "react-icons/si";
+import { SiGit, SiJavascript, SiNextdotjs, SiPostgresql, SiPostman, SiReact, SiRedux, SiSupabase, SiTailwindcss,SiTypescript, SiExpress, SiNodedotjs, SiMongodb } from "react-icons/si";
 
 export default function Skills() {
     const skills = [{
-        text:"React",
+        text:"React.js",
         Icon: SiReact
     },
     {
@@ -19,11 +19,14 @@ export default function Skills() {
         text:"Supabase",
         Icon: SiSupabase
     },{
-        text:"Postgres",
-        Icon: SiPostgresql
+        text:"MongoDb",
+        Icon: SiMongodb
     },{
         text:"Javascript",
         Icon: SiJavascript
+    },{
+        text:"Typescript",
+        Icon:SiTypescript
     },{
         text:"Redux",
         Icon:SiRedux
@@ -33,6 +36,12 @@ export default function Skills() {
     },{
         text:"Git",
         Icon: SiGit
+    },{
+        text:"Node.js",
+        Icon:SiNodedotjs
+    },{
+        text:"Express.js",
+        Icon:SiExpress
     }]
   return (
     <div className="max-w-5xl mx-auto px-8">
--- lib/project.ts
+++ lib/project.ts
@@ -1,5 +1,5 @@
 import { IconType } from "react-icons";
-import { SiMongodb, SiNextdotjs, SiNodedotjs, SiStripe, SiSupabase, SiTailwindcss, SiTypescript } from "react-icons/si";
+import { SiMongodb, SiNextdotjs, SiNodedotjs, SiReact, SiStripe, SiSupabase, SiTailwindcss, SiTypescript } from "react-icons/si";
 
 export interface Project {
     title: string
@@ -9,12 +9,18 @@ export interface Project {
     background: string
 }
 export const data: Project[] = [{
+    title: 'Formiverse',
+    tech: [SiReact, SiNodedotjs, SiTypescript, SiMongodb, SiTailwindcss],
+    link: 'https://formiverse.vercel.app/',
+    cover: '/form.png',
+    background: 'bg-indigo-500'
+}, {
     title: 'Dev Vault',
     tech: [SiNextdotjs, SiNodedotjs, SiTypescript, SiMongodb, SiTailwindcss],
     link: 'https://dev-vault-preet006s-projects.vercel.app/',
     cover: '/pic2.png',
-    background: 'bg-indigo-500'
-}, {
+    background: 'bg-green-500'
+},{
     title: 'Tech Blogs SAAS',
     tech: [SiNextdotjs, SiTypescript, SiSupabase, SiTailwindcss, SiStripe],
     link: 'https://tech-blogs2.vercel.app/',
